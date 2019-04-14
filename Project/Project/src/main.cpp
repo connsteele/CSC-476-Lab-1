fdaca54908638c35eb3332a593a7062d17b15b70
con <connsteele96@gmail.com>
2019-04-13 19:13:24 -0700

    Added libraries
    Added GLM, GLFW via Nuget Package Manager
    Added tiny_obj_loader and stb_img and glad

---
+++ Project/Project/src/main.cpp
@@ -0,0 +1,11 @@
+#include "glad/glad.h"
+
+#define TINYOBJLOADER_IMPLEMENTATION
+#include "tiny_obj_loader.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+int main() {
+
+	return 0;
+}
